305315637910e5401ef12bbe0d3ad65cc5dab608
Aaron Lin <aaron_lin@wistron.com>
2019-08-15 09:25:14 +0800

    Hello

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,16 +1,48 @@
 import React, {
+  Fragment,
   useState,
   useEffect,
 } from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import {
   generateCitizenId,
   generateForeignerId,
 } from '../utils/functions';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
 // 一次產生幾組
 const randomCount = 5;
+
+const ListRow = (props) => {
+  const { text } = props;
+  
+  return (
+    <li style={{ height: '35px' }}>
+      <div key={text} style={{ display: 'flex' }}>
+        <div style={{ paddingRight: '10px', paddingTop: '5px', width: '100px' }}>{ text }</div>
+        <CopyToClipboard text={text}>
+          <Button variant="outlined" color="secondary" size="small">Copy</Button>
+        </CopyToClipboard>
+      </div>
+    </li>
+  );
+}
 
 const Tools = () => {
   const [ids, setIds] = useState([]);
@@ -43,13 +75,13 @@ const Tools = () => {
       <p>身分證字號</p>
       <ul>
         {
-          ids.map(id => <li>{id}</li>)
+          ids.map(id => (<ListRow text={id} />))
         }
       </ul>
       <p>拘留證號</p>
       <ul>
         {
-          livingIds.map(livingId => <li>{livingId}</li>)
+          livingIds.map(livingId => (<ListRow text={livingId} />))
         }
       </ul>
     </>
